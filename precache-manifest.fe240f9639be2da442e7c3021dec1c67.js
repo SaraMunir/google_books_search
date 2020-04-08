04af0fbf26508a822c02716cb0e9fbbc98952e80
Sara Munir <saramunir011@gmail.com>
2020-04-07 20:00:27 -0400

    Updates

---
+++ precache-manifest.fe240f9639be2da442e7c3021dec1c67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5dc7cffeb5743729cb365c2a76e2096",
+    "url": "/google_books_search/index.html"
+  },
+  {
+    "revision": "8171f941a06bb0c71595",
+    "url": "/google_books_search/static/css/main.b693bb73.chunk.css"
+  },
+  {
+    "revision": "930606e3eec7665ab6ae",
+    "url": "/google_books_search/static/js/2.a64986ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google_books_search/static/js/2.a64986ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8171f941a06bb0c71595",
+    "url": "/google_books_search/static/js/main.d1850d1d.chunk.js"
+  },
+  {
+    "revision": "ea7033f63f4f87af40b5",
+    "url": "/google_books_search/static/js/runtime-main.06b60de0.js"
+  },
+  {
+    "revision": "0d1c866779afd78e062df1d3e88b0eda",
+    "url": "/google_books_search/static/media/1200px-Google_Books_logo_2015.svg.0d1c8667.png"
+  },
+  {
+    "revision": "0c5b914fac708c592bf2d2f32f992e9f",
+    "url": "/google_books_search/static/media/janko-ferlic-sfL_QOnmy00-unsplash.0c5b914f.jpg"
+  }
+]);
